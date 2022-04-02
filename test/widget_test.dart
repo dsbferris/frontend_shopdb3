02cacf8316df33ee26365cf910de39cacb83d4a7
Ferris Schwanebeck <ferris.s@aol.de>
2022-04-02 12:42:21 +0200

    Little naming error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shopdb3/main.dart';
+import 'package:shopdb3_frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
